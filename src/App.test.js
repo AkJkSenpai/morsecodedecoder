c4c73148f8a616221f86acfeb39ff94381690d53
Aku Paananen <aku.k.paananen@utu.fi>
2023-01-28 18:21:50 +0200

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,144 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const headerElement = screen.getByText('Morsecode decoder');
+    expect(headerElement).toBeInTheDocument();
+});
+
+const dummyCodes = [
+    {
+        code: '*',
+        expected: 'E'
+    },
+    {
+        code: '-',
+        expected: 'T'
+    },
+    {
+        code: '**',
+        expected: 'I'
+    },
+    {
+        code: '*-',
+        expected: 'A'
+    },
+    {
+        code: '-*',
+        expected: 'N'
+    },
+    {
+        code: '--',
+        expected: 'M'
+    },
+    {
+        code: '***',
+        expected: 'S'
+    },
+    {
+        code: '**-',
+        expected: 'U'
+    },{
+        code: '*-*',
+        expected: 'R'
+    },
+    {
+        code: '*--',
+        expected: 'W'
+    },
+    {
+        code: '-**',
+        expected: 'D'
+    },
+    {
+        code: '-*-',
+        expected: 'K'
+    },
+    {
+        code: '--*',
+        expected: 'G'
+    },
+    {
+        code: '---',
+        expected: 'O'
+    },
+    {
+        code: '****',
+        expected: 'H'
+    },
+    {
+        code: '***-',
+        expected: 'V'
+    },
+    {
+        code: '**-*',
+        expected: 'F'
+    },
+    {
+        code: '**--',
+        expected: 'Ü'
+    },
+    {
+        code: '*-**',
+        expected: 'L'
+    },
+    {
+        code: '*-*-',
+        expected: 'Ä'
+    },
+    {
+        code: '*--*',
+        expected: 'P'
+    },
+    {
+        code: '*---',
+        expected: 'J'
+    },
+    {
+        code: '-***',
+        expected: 'B'
+    },
+    {
+        code: '-**-',
+        expected: 'X'
+    },
+    {
+        code: '-*-*',
+        expected: 'C'
+    },
+    {
+        code: '-*--',
+        expected: 'Y'
+    },
+    {
+        code: '--**',
+        expected: 'Z'
+    },
+    {
+        code: '--*-',
+        expected: 'Q'
+    },
+    {
+        code: '---*',
+        expected: 'Ö'
+    },
+    {
+        code: '----',
+        expected: 'Š'
+    },
+];
+
+test('decoding morsecode', () => {
+    render(<App />);
+
+    const inputField = screen.getByTestId('inputField');
+    const submitButton = screen.getByTestId('submitButton');
+    const code = screen.getByTestId('code');
+
+    dummyCodes.forEach(element => {
+        fireEvent.change(inputField, { target: { value: element.code} })
+        fireEvent.click(submitButton);
+        expect(code).toHaveTextContent(element.expected);
+    });
 });
